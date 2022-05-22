1fe86b37e94c25b19c71f1036a4455569326d4d3
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-05-22 07:06:50 -0400

    Currently working on styling the data retrieved from the Github api

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 let input = document.querySelector(".input__group__field");
 let btn = document.querySelector(".input__group__submit-btn");
-let username = document.querySelector(".username");
+let username = document.querySelector(".result__username");
 let submit_btn = document.querySelector(".submit_btn");
-let personName = document.querySelector(".person_name");
+let personName = document.querySelector(".result__person-name");
 let userName = document.querySelector(".username");
-let joinStatus = document.querySelector(".join_status");
+let joinStatus = document.querySelector(".result__join_status");
 let repoNumber = document.querySelector(".repo_number");
 let followersNumber = document.querySelector(".followers_number");
 let repoDescription = document.querySelector(".repo_description");
@@ -16,34 +16,33 @@ let userTwitter = document.querySelector(".user_twitter");
 let userCompany = document.querySelector(".user_company");
 let userTwitterUsername = document.querySelector(".user_twitter");
 let img = document.createElement("img");
-let personAvatar = document.querySelector(".user_avatar");
-
+let personAvatar = document.querySelector(".result__user-avatar");
 
 btn.addEventListener("click", () => {
   async function getUser() {
-    let value = input.value;  
-    const response = await fetch(`https://api.github.com/users/${value}`)
-    const data = await response.json()
-    
+    let value = input.value;
+    const response = await fetch(`https://api.github.com/users/${value}`);
+    const data = await response.json();
+
     console.log(data.created_at);
 
     img.src = data.avatar_url;
     personAvatar.appendChild(img);
     personAvatar.style.border = "none";
-        
+
     const dateData = data.created_at.slice(0, data.created_at.length - 10);
 
     personName.innerHTML = data.name;
     username.innerHTML = data.login;
-    joinStatus.innerHTML = `Joined ${dateData}` ;
-    profileDescription.innerHTML = data.bio;
-    repoNumber.innerHTML = data.public_repos;
-    followersNumber.innerHTML = data.followers;
-    followingNumber.innerHTML = data.following; 
-    userLocation.innerHTML = data.location;
-    userBlog.innerHTML = data.blog;         
-    userCompany.innerHTML = data.company;
-    userTwitterUsername.innerHTML = data.twitter_username;
+    joinStatus.innerHTML = `Joined ${dateData}`;
+    // profileDescription.innerHTML = data.bio;
+    // repoNumber.innerHTML = data.public_repos;
+    // followersNumber.innerHTML = data.followers;
+    // followingNumber.innerHTML = data.following;
+    // userLocation.innerHTML = data.location;
+    // userBlog.innerHTML = data.blog;
+    // userCompany.innerHTML = data.company;
+    // userTwitterUsername.innerHTML = data.twitter_username;
   }
   getUser();
 });
